42aaf6cdf604dbfd57711b5c66a70b583dc78d53
bikash <shresthabikash2073@gmail.com>
2021-12-20 21:17:02 +0545

    btn clicked event listener added by implementing ActionListener interface

---
+++ JavaTest/GUI/ClickEventExample.java
@@ -0,0 +1,26 @@
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class ClickEventExample extends Frame implements ActionListener {
+    TextField tf;
+    ClickEventExample(){
+        tf = new TextField();
+        tf.setBounds(50, 100, 80, 20);
+        Button btn = new Button("click me");
+        btn.setBounds(50, 200, 80, 20);
+        btn.addActionListener(this);
+        add(btn);
+        add(tf);
+        setSize(300, 400);
+        setVisible(true);
+        setLayout(null);
+    }
+    public void actionPerformed(ActionEvent e){
+        tf.setText("Hello Bikash");
+    }
+
+    public static void main(String args[]){
+        ClickEventExample obj = new ClickEventExample();
+    }
+}
