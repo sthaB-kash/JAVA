b54494e0da855b98e6a5020233803a837d252595
bikash <shresthabikash2073@gmail.com>
2021-12-21 17:53:11 +0545

    java packages

---
+++ JavaTest/JavaPackages/B.java
@@ -0,0 +1,22 @@
+package mypack;
+
+//using packagename.*;
+// import pack.*;
+
+//using packagename.className
+// import pack.A;
+
+public class B {
+    public static void main(String args[]){
+        // A obj = new A();
+
+        //using fully qualified name 
+        pack.A obj = new pack.A();
+        obj.msg();
+    }
+}
+
+
+
+
+ 
